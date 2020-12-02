b5199b6d930cd0427fc114450316da6731930056
Jamie Hill <jalhill91@gmail.com>
2020-12-01 21:33:27 -0500

    initial commit

--- DevTeamsProject/Developer.cs
+++ DevTeamsProject/Developer.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace DevTeamsProject
 {
+    //public enum DevGroup
+    //{
+    //    DevGroup1 = 1,
+    //    DevGroup2,
+    //    DevGroup3,
+    //    DevGroup4
+    //}
     public class Developer
     {
+        public string Name { get; set; }
+        public string DevID { get; set; }
+        public bool PluralSightAccess { get; set; }
     }
+
+    public Developer() { }
+
+    public Developer(string name, string devID, bool pluralSightAccess)
+    {
+        Name = name;
+        DevID - devID;
+        PluralSightAccess = pluralSightAccess;
+    }
+
 }
--- DevTeamsProject/DeveloperRepo.cs
+++ DevTeamsProject/DeveloperRepo.cs
@@ -11,10 +11,59 @@ namespace DevTeamsProject
         private readonly List<Developer> _developerDirectory = new List<Developer>();
 
         //Developer Create
-        //Developer Read
+        public void AddContentToList(Developer.content)
+        {
+            _developerDirectory.Add(AddContent);
+        }
+        //Developer Read    
+        public List<Developer> GetContentList()
+        {
+            return _developerDirectory;
+        }
         //Developer Update
+        public void updateExistingContent(String originaDevID, DeveloperRepo newContent)
+
+        // Find the Content
+        Developer oldContent = GetContentByDevID(originalDevID);
+
+        // Update the content
+        if (oldContent != null)
+           {
+            oldContent.Name = newContent.Name;
+            oldContent.DevID = newContent.DevID;
+            oldContent.PluralSightAccess = newCotent.PluralSightAccess;
+            }
         //Developer Delete
+        pulic bool RemoveContentFromList(string devID)
+        {
+            Developer content = GetContentByDevID(devID)
+                if (content == null)
+            {
+                return false;
+            }
+            int initialCount = +_developerDirectory.content;
+            _listofContent.Remove(content);
+
+            if (initialCount > _listOfContent.count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         //Developer Helper (Get Developer by ID)
+        private Developer GetContentby(string devID)
+        {
+            for each(Developer content in _developerDirectory)
+                if (content.DevID == devID)
+                {
+                    return GetContent;
+                }
+        }
+
+        return null;
     }
 }
